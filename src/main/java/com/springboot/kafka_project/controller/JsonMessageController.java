a025453ab8330966f315ee017e7565f35ec7eba4
Hemanth <hemanthkumar882001@gmail.com>
2026-06-28 00:01:30 +0530

    commit-Kafka-Demo Project

---
+++ src/main/java/com/springboot/kafka_project/controller/JsonMessageController.java
@@ -0,0 +1,26 @@
+package com.springboot.kafka_project.controller;
+
+import com.springboot.kafka_project.kafkaproducer.JsonKafkaProducer;
+import com.springboot.kafka_project.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer jsonKafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+}
---
+++ src/main/java/com/springboot/kafka_project/kafkaconsumer/JsonKafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.springboot.kafka_project.kafkaconsumer;
+
+import com.springboot.kafka_project.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(User user){
+        logger.info(String.format("Json message received -> %s",user.toString()));
+    }
+}
